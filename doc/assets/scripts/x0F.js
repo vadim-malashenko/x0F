6d7a531e66bacd924be5acb5c01b20d14987488f
Vadim Malashenko <zex404@gmail.com>
2024-09-22 00:13:26 +1000

    Add canvas.

--- doc/assets/scripts/x0F.js
+++ doc/assets/scripts/x0F.js
@@ -1,12 +1,30 @@
 export default class x0F
 {
+    canvas
+    context
+
     constructor()
     {
-        
+        this.canvas = this.getCanvas(`x0f`)
+        this.context = this.getContext(this.canvas)
+    }
+
+    getCanvas(id)
+    {
+        window[id].width = innerWidth
+        window[id].height = innerHeight
+        return window[id]
+    }
+
+    getContext(canvas)
+    {
+        context = canvas.getContext("2d")
+        context.font = "16px Arial"
+        return context
     }
 
     static load(ev)
     {
-        console.log(this.constructor.name)
+        ev.target.x0F = ev.target.x0F ?? new x0F()
     }
 }
